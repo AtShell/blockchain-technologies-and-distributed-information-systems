0815b8974bc622c456cbde819b2d81bc1b13ca7d
Вячеслав <76532024+AtShell@users.noreply.github.com>
2024-03-12 21:22:34 +0500

    Add files via upload

---
+++ 1 laba/lr1.2/Program.cs
@@ -0,0 +1,140 @@
+﻿using static System.Console;
+
+
+var lock1 = new object();
+var lock2 = new object();
+
+void AcquireLocks1()
+{
+    var threadId = Thread.CurrentThread.ManagedThreadId;
+
+    lock (lock1)
+    {
+        WriteLine($"Thread {threadId} acquired lock 1.");
+        Thread.Sleep(1000);
+        WriteLine($"Thread {threadId} attempted to acquire lock 2.");
+        lock (lock2)
+        {
+            WriteLine($"Thread {threadId} acquired lock 2.");
+        }
+    }
+}
+
+void AcquireLocks2()
+{
+    var threadId = Thread.CurrentThread.ManagedThreadId;
+
+    lock (lock2)
+    {
+        WriteLine($"Thread {threadId} acquired lock 2.");
+        Thread.Sleep(1000);
+        WriteLine($"Thread {threadId} attempted to acquire lock 1.");
+        lock (lock1)
+        {
+            WriteLine($"Thread {threadId} acquired lock 1.");
+        }
+    }
+}
+
+var thread1 = new Thread(AcquireLocks1);
+var thread2 = new Thread(AcquireLocks2);
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+WriteLine("Finished.");
+ReadLine();
+
+//using static System.Console;
+
+//var lock1 = new object();
+//var lock2 = new object();
+
+//void AcquireLocks1()
+//{
+//    var threadId = Thread.CurrentThread.ManagedThreadId;
+
+//    while (true)
+//    {
+//        if (Monitor.TryEnter(lock1, TimeSpan.FromSeconds(1)))
+//        {
+//            try
+//            {
+//                WriteLine($"Thread {threadId} acquired lock 1.");
+//                Thread.Sleep(1000);
+//                WriteLine($"Thread {threadId} attempted to acquire lock 2.");
+
+//                if (Monitor.TryEnter(lock2, TimeSpan.FromSeconds(1)))
+//                {
+//                    try
+//                    {
+//                        WriteLine($"Thread {threadId} acquired lock 2.");
+//                        break;
+//                    }
+//                    finally
+//                    {
+//                        Monitor.Exit(lock2);
+//                    }
+//                }
+//            }
+//            finally
+//            {
+//                Monitor.Exit(lock1);
+//            }
+//        }
+//    }
+
+//    WriteLine($"Thread {threadId} is done.");
+//}
+
+//void AcquireLocks2()
+//{
+//    var threadId = Thread.CurrentThread.ManagedThreadId;
+
+//    while (true)
+//    {
+//        if (Monitor.TryEnter(lock2, TimeSpan.FromSeconds(1)))
+//        {
+//            try
+//            {
+//                WriteLine($"Thread {threadId} acquired lock 2.");
+//                Thread.Sleep(1000);
+//                WriteLine($"Thread {threadId} attempted to acquire lock 1.");
+
+//                if (Monitor.TryEnter(lock1, TimeSpan.FromSeconds(1)))
+//                {
+//                    try
+//                    {
+//                        WriteLine($"Thread {threadId} acquired lock 1.");
+//                        break;
+//                    }
+//                    finally
+//                    {
+//                        Monitor.Exit(lock1);
+//                    }
+//                }
+//            }
+//            finally
+//            {
+//                Monitor.Exit(lock2);
+//            }
+//        }
+//    }
+
+//    WriteLine($"Thread {threadId} is done.");
+//}
+
+//var thread1 = new Thread(AcquireLocks1);
+//var thread2 = new Thread(AcquireLocks2);
+
+//thread1.Start();
+//thread2.Start();
+
+//thread1.Join();
+//thread2.Join();
+
+//WriteLine("Finished.");
+//ReadLine();
---
+++ 1 laba/lr1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+int y = 0;
+object locker = new();
+int x = 0;
+Console.WriteLine("\nProblematic\n");
+problematic();
+Thread.Sleep(500);
+Console.WriteLine("\nSolution\n");
+solution();
+void problematic()
+{
+
+    for (int i = 1; i < 6; i++)
+    {
+        Thread myThread = new(Print);
+        myThread.Name = $"Thread {i}";
+        myThread.Start();
+    }
+}
+void solution()
+{
+
+    for (int i = 1; i < 6; i++)
+    {
+        Thread myThread = new(PrintLock);
+        myThread.Name = $"Thread {i}";
+        myThread.Start();
+    }
+}
+void Print()
+{
+    x = 1;
+    for (int i = 1; i < 6; i++)
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+        x++;
+    }
+}
+void PrintLock()
+{
+    lock (locker)
+    {
+        y = 1;
+        for (int i = 1; i < 6; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {y}");
+            y++;
+        }
+    }
+}
